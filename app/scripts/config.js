2bc6d3fcd2da65bcebf34a0883271e254cb3bc88
santanubankura <santanu55.bankura@gmail.com>
2017-03-20 12:15:58 +0530

    initial commit

---
+++ app/scripts/config.js
@@ -0,0 +1,315 @@
+/**
+ * HOMER - Responsive Admin Theme
+ * version 1.8
+ *
+ */
+
+function configState($stateProvider, $urlRouterProvider, $compileProvider) {
+
+    // Optimize load start with remove binding information inside the DOM element
+    $compileProvider.debugInfoEnabled(true);
+
+    // Set default state
+    $urlRouterProvider.otherwise("/landing_page");
+    $stateProvider
+
+        // Landing page
+        .state('landing', {
+            url: "/landing_page",
+            templateUrl: "views/landing_page.html",
+            data: {
+                pageTitle: 'Landing page',
+                specialClass: 'landing-page'
+            }
+        })
+
+        // Dashboard - Main page
+        .state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "views/dashboard.html",
+            data: {
+                pageTitle: 'Dashboard'
+            }
+        })
+        .state('addproduct', {
+            url: "/addproduct",
+            templateUrl: "views/add-product.html",
+            data: {
+                pageTitle: 'Add Product'
+            }
+        })
+        .state('viewproducts', {
+            url: "/viewproducts",
+            templateUrl: "views/viewproducts.html",
+            data: {
+                pageTitle: 'View Product'
+            }
+        })
+        
+        // Interface
+        .state('interface', {
+            abstract: true,
+            url: "/interface",
+            templateUrl: "views/common/content.html",
+            data: {
+                pageTitle: 'Interface'
+            }
+        })
+        .state('interface.buttons', {
+            url: "/buttons",
+            templateUrl: "views/interface/buttons.html",
+            data: {
+                pageTitle: 'Colors and Buttons',
+                pageDesc: 'The basic color palette'
+            }
+        })
+        .state('interface.typography', {
+            url: "/typography",
+            templateUrl: "views/interface/typography.html",
+            data: {
+                pageTitle: 'Typography',
+                pageDesc: 'The basic elements of typography'
+            }
+        })
+        .state('interface.components', {
+            url: "/components",
+            templateUrl: "views/interface/components.html",
+            data: {
+                pageTitle: 'Components',
+                pageDesc: 'Tabs, according, collapse and other UI components'
+            }
+        })
+        .state('interface.icons', {
+            url: "/icons",
+            templateUrl: "views/interface/icons.html",
+            data: {
+                pageTitle: 'Icons',
+                pageDesc: 'Two great icon libraries. Pe-icon-7-stroke and Font Awesome'
+            }
+        })
+        .state('interface.panels', {
+            url: "/panels",
+            templateUrl: "views/interface/panels.html",
+            data: {
+                pageTitle: 'Panels',
+                pageDesc: 'Two great icon libraries. Pe-icon-7-stroke and Font Awesome'
+            }
+        })
+        .state('interface.alerts', {
+            url: "/alerts",
+            templateUrl: "views/interface/alerts.html",
+            data: {
+                pageTitle: 'Alerts',
+                pageDesc: 'Notification and custom alerts'
+            }
+        })
+        .state('interface.modals', {
+            url: "/modals",
+            templateUrl: "views/interface/modals.html",
+            data: {
+                pageTitle: 'Modals',
+                pageDesc: 'Modal window examples'
+            }
+        })
+        .state('interface.loading_buttons', {
+            url: "/loading_buttons",
+            templateUrl: "views/interface/loading_buttons.html",
+            data: {
+                pageTitle: 'Ladda',
+                pageDesc: 'Loading buttons'
+            }
+        })
+        .state('interface.list', {
+            url: "/list",
+            templateUrl: "views/interface/list.html",
+            data: {
+                pageTitle: 'Nestable list',
+                pageDesc: 'Nestable - Drag & drop hierarchical list.'
+            }
+        })
+
+        .state('interface.tour', {
+            url: "/tour",
+            templateUrl: "views/interface/tour.html",
+            data: {
+                pageTitle: 'Tour',
+                pageDesc: 'The easiest way to show people how to use your website.'
+            }
+        })
+
+        .state('interface.draggable_panels', {
+            url: "/draggable_panels",
+            templateUrl: "views/interface/draggable_panels.html",
+            data: {
+                pageTitle: 'Draggable panels',
+                pageDesc: 'Example page for draggable panels'
+            }
+        })
+
+        .state('interface.code_editor', {
+            url: "/code_editor",
+            templateUrl: "views/interface/code_editor.html",
+            data: {
+                pageTitle: 'Code editor',
+                pageDesc: 'Versatile text editor implemented in JavaScript for the browser.'
+            }
+        })
+
+        // Common views
+        .state('common', {
+            abstract: true,
+            url: "/common",
+            templateUrl: "views/common/content_empty.html",
+            data: {
+                pageTitle: 'Common'
+            }
+        })
+        .state('common.login', {
+            url: "/login",
+            templateUrl: "views/common_app/login.html",
+            data: {
+                pageTitle: 'Login page',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.register', {
+            url: "/register",
+            templateUrl: "views/common_app/register.html",
+            data: {
+                pageTitle: 'Register page',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.error_one', {
+            url: "/error_one",
+            templateUrl: "views/common_app/error_one.html",
+            data: {
+                pageTitle: 'Error 404',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.error_two', {
+            url: "/error_two",
+            templateUrl: "views/common_app/error_two.html",
+            data: {
+                pageTitle: 'Error 505',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.lock', {
+            url: "/lock",
+            templateUrl: "views/common_app/lock.html",
+            data: {
+                pageTitle: 'Lock page',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.password_recovery', {
+            url: "/password_recovery",
+            templateUrl: "views/common_app/password_recovery.html",
+            data: {
+                pageTitle: 'Password recovery',
+                specialClass: 'blank'
+            }
+        })
+        // Tables views
+        .state('tables', {
+            abstract: true,
+            url: "/tables",
+            templateUrl: "views/common/content.html",
+            data: {
+                pageTitle: 'Tables'
+            }
+        })
+        .state('tables.tables_design', {
+            url: "/tables_design",
+            templateUrl: "views/tables/tables_design.html",
+            data: {
+                pageTitle: 'Tables design',
+                pageDesc: 'Examples of various designs of tables.'
+            }
+        })
+        .state('tables.ng_grid', {
+            url: "/ng_grid",
+            templateUrl: "views/tables/ng_grid.html",
+            data: {
+                pageTitle: 'ngGgrid',
+                pageDesc: 'Examples of various designs of tables.'
+            }
+        })
+        .state('tables.datatables', {
+            url: "/datatables",
+            templateUrl: "views/tables/datatables.html",
+            data: {
+                pageTitle: 'DataTables',
+                pageDesc: 'Advanced interaction controls to any HTML table'
+            }
+        })
+        .state('tables.footable', {
+            url: "/footable",
+            templateUrl: "views/tables/footable.html",
+            data: {
+                pageTitle: 'FooTable',
+                pageDesc: 'Advanced interaction controls to any HTML table'
+            }
+        })
+
+        // Forms views
+        .state('forms', {
+            abstract: true,
+            url: "/forms",
+            templateUrl: "views/common/content_small.html",
+            data: {
+                pageTitle: 'Forms'
+            }
+        })
+        .state('forms.forms_elements', {
+            url: "/forms_elements",
+            templateUrl: "views/forms/forms_elements.html",
+            data: {
+                pageTitle: 'Forms elements',
+                pageDesc: 'Examples of various form controls.'
+            }
+        })
+        .state('forms.forms_extended', {
+            url: "/forms_extended",
+            templateUrl: "views/forms/forms_extended.html",
+            data: {
+                pageTitle: 'Forms extended',
+                pageDesc: 'Examples of various extended form controls.'
+            }
+        })
+        .state('forms.text_editor', {
+            url: "/text_editor",
+            templateUrl: "views/forms/text_editor.html",
+            data: {
+                pageTitle: 'Text editor',
+                pageDesc: 'Examples of text editor.'
+            }
+        })
+        .state('forms.wizard', {
+            url: "/wizard",
+            templateUrl: "views/forms/wizard.html",
+            data: {
+                pageTitle: 'Wizard',
+                pageDesc: 'Build a form with wizard functionality.'
+            }
+        })
+
+        .state('forms.validation', {
+            url: "/validation",
+            templateUrl: "views/forms/validation.html",
+            data: {
+                pageTitle: 'Validation',
+                pageDesc: 'Build a form with validation functionality.'
+            }
+        })
+}
+
+angular
+    .module('homer')
+    .config(configState)
+    .run(function($rootScope, $state, editableOptions) {
+        $rootScope.$state = $state;
+        editableOptions.theme = 'bs3';
+    });
